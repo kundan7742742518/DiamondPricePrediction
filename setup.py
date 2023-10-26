e118480f4208ed1ad1db8c57cb4a4c9bf9ed97db
kundan7742742518 <jangirkundan540@gmail.com>
2023-10-26 10:45:16 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages , setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(filepath:str)->List[str]:
+    requirements = []
+    with open(filepath) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+        
+        return requirements
+
+
+
+
+setup(
+    name = 'DiamendPriceprediction',
+    version='0.0.1',
+    author='Pwskills',
+    author_email="jangirkundan540@gmail.com",
+    install_requires = get_requirements('requirements.txt'),
+    packages=find_packages(),
+    
+    
+)
